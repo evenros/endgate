df75a7cf9ed7a62bdd6541f3a984fb835079c77f
Sprite <noreply@sprite.dev>
2026-01-29 19:27:41 +0000

    restart

--- test/engineering_analytics_terminal_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule EngineeringAnalyticsTerminalTest do
-  use ExUnit.Case
-  doctest EngineeringAnalyticsTerminal
-
-  test "greets the world" do
-    assert EngineeringAnalyticsTerminal.hello() == :world
-  end
-end
